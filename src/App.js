b46773904eafcfe6806274148c6f5983b669f35b
Monzurulislampro <mounzrul@gmail.com>
2022-12-12 09:00:34 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+// get data from mongodb database
+const App = () => {
+  const [userInfo, setuserInfo] = useState('');
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      const data = await axios.get("http://localhost:5000/post");
+      setuserInfo(data);
+      console.log(data)
+    }
+    fetchData();
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className = "App">
+    <h1> Fetch data from an api in react </h1>  {
+        userInfo && userInfo?.data.map((item) => ( 
+        <main>
+          <div>
+          <ol key = { item._id } >
+            <ul>User_Name: { item.name }</ul> 
+            <ul>User_Email: { item.email } </ul>
+            <h1> how are you</h1>
+            </ol>
+          </div>
+        </main>
+        ))
+    }
+</div>
+);
 }
+
 
 export default App;
